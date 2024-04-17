9f74118efd6c8379225099c4dd3eed5d0e21abac
chelu <cheluhg@gmail.com>
2024-04-17 19:29:35 +0200

    cambio de rutas

--- client/src/Routes/AppRoutes.jsx
+++ client/src/Routes/AppRoutes.jsx
@@ -4,6 +4,8 @@ import { ErrorPage } from "../pages/dashboard/errorPage/ErrorPage";
 import { Home } from "../pages/dashboard/home/Home";
 import NavBarApp from "../components/NavBarApp/NavBarApp";
 import { Contacto } from "../pages/dashboard/contacto/Contacto";
+import Cv from "../pages/dashboard/cv/Cv";
+import Projects from "../pages/dashboard/proyectos/Projects";
 
 export const AppRoutes = () => {
   return (
@@ -11,8 +13,10 @@ export const AppRoutes = () => {
       <NavBarApp />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<ErrorPage />} />
         <Route path="/contacto" element={<Contacto />} />
+        <Route path="/cv" element={<Cv />} />
+        <Route path="/proyectos" element={<Projects />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
